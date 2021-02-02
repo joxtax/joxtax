148d95c78d7f2b817cd1b59b9ccd39f26b2fc270
joxtax <77255187+joxtax@users.noreply.github.com>
2021-02-02 10:27:25 -0400

    miercoles a las 6pm

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2021, // required
 	month: 2, // required
-	day: 2, // required
-	hours: 18, // Default is 0 [0-23] integer
+	day: 3, // required
+	hours: 14, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
